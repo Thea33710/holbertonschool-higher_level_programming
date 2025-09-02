bbf343fce4ef4efb2b6d9c2b99ec70fb75e84eef
Thea33710 <thea33710@gmail.com>
2025-09-02 16:09:53 +0200

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
 
     if argc == 0:
         print("0 arguments.")
-
+    elif argc == 1:
+        print("1 argument:")
     else:
         print("{} arguments:".format(argc))
 
