75a2917b66a9dfd43820bec23a0b170f01379cec
Thea33710 <thea33710@gmail.com>
2025-09-02 14:30:10 +0200

    task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,10 +4,8 @@ while a <= 8:
     b = a + 1
     while b <= 9:
         if a == 8 and b == 9:
-            print(f"{a}{b}")
-
+            print("{}{}".format(a, b))
         else:
-            print(f"{a}{b}, ", end="")
-
+            print("{}{}, ".format(a, b), end="")
         b += 1
     a += 1
