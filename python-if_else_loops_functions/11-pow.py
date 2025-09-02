f2b3421f9db7a6c0338400d94e54c25a6b76f57c
Thea33710 <thea33710@gmail.com>
2025-09-02 14:42:56 +0200

    changement f

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,10 +1,3 @@
 #!/usr/bin/env python3
 def pow(a, b):
-    result = 1
-    if b < 0:
-        a = 1 / a
-        b = -b
-    while b > 0:
-        result *= a
-        b -= 1
-    return result
+    return a ** b
