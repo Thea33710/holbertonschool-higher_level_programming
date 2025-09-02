6991f4eeead1fc4a11a8593ce1a8ae8797764be4
Thea33710 <thea33710@gmail.com>
2025-09-02 13:08:13 +0200

    task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(100):
-    print("{:02d}".format(i) if i == 99 else "{:02d}, ".format(i), end="")
+i = 0
+while i < 99:
+    print("{:02d}, ".format(i), end="")
+    i += 1
+print("{:02d}".format(i))
