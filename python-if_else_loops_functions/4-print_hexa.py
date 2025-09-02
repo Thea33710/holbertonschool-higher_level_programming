1ca02f1fb6784926c071bbbd207658cb83cba1f3
Thea33710 <thea33710@gmail.com>
2025-09-02 13:21:43 +0200

    task 4

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-
-i = 0
-while i <= 98:
-    if i == 98:
-        print("{:d} = 0x{:x}".format(i, i))
-    else:
-        print("{:d} = 0x{:x}, ".format(i, i), end="")
-    i += 1
+for i in range(99):
+    print(f"{i} = 0x{i:x}")
