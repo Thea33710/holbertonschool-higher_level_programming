89e9d9f6d4584c2a7d9bd46427272f2c8305e2f2
Thea33710 <thea33710@gmail.com>
2025-09-08 13:10:33 +0200

    changements

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,9 +3,9 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
 
-    max = 0
+    big = my_list[0]
     for i in range(len(my_list)):
-        if my_list[i] > max:
-            max = my_list[i]
+        if my_list[i] > big:
+            big = my_list[i]
 
-    return max
+    return big
