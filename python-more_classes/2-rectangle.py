9e5bb028e381bbe532c4771728f1e8aa1081f217
Thea33710 <thea33710@gmail.com>
2025-09-15 11:57:54 +0200

    task 2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -98,4 +98,7 @@ class Rectangle:
 
         """Returns the perimeter of the rectangle."""
 
-        return ((self.__width * 2) + (self.__height * 2))
+        if self.__width == 0 or self.__height == 0:
+            return 0
+
+        return 2 * (self.__width + self.__height)
