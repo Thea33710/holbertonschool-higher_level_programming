957a0cea8aeef5dfaac63569c7628e89cf2598c8
Thea33710 <thea33710@gmail.com>
2025-09-22 15:01:13 +0200

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class BaseGeometry:
+
+    """A geometry class."""
+
+    def area(self):
+
+        """Raises an Exception with a message."""
+
+        raise Exception("area() is not implemented")
