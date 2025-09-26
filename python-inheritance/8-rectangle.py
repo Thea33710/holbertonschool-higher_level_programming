0726ef050a4443e8990d66620db25f6289ffb1b7
Thea33710 <thea33710@gmail.com>
2025-09-26 12:54:41 +0200

    task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -51,8 +51,8 @@ class Rectangle(BaseGeometry):
             height (int): The height of the rectangle.
         """
 
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
 
         self.__width = width
         self.__height = height
