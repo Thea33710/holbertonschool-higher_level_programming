52052934994b6803f3a9a0e1702e724089aac3b8
Thea33710 <thea33710@gmail.com>
2025-09-26 11:18:51 +0200

    task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -4,17 +4,13 @@
 
 
 class BaseGeometry:
-
     """A geometry class."""
 
     def area(self):
-
         """Raises an Exception with a message."""
-
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-
         """
         Validates value.
 
@@ -26,7 +22,6 @@ class BaseGeometry:
             TypeError: If value is not a integer.
             ValueError: If value is less or equal to 0.
         """
-
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
 
@@ -34,15 +29,10 @@ class BaseGeometry:
             raise ValueError(f"{name} must be greater than 0")
 
 
-"""A rectangle class."""
-
-
 class Rectangle(BaseGeometry):
-
     """A class that inherits from BaseGeometry"""
 
     def __init__(self, width, height):
-
         """
         Initializes a rectangle with a given width and height.
 
@@ -56,34 +46,24 @@ class Rectangle(BaseGeometry):
             TypeError: If height is not an integer.
             ValueError: If height is less than 0.
         """
-
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
-
         """Returns the area of the rectangle."""
-
         return self.__width * self.__height
 
     def __str__(self):
-
         """Return the rectangle description: [Rectangle] <width>/<height>."""
-
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
 
-"""A square class."""
-
-
 class Square(Rectangle):
-
     """A class that inherits from Rectangle."""
 
     def __init__(self, size):
-
         """
         Initializes a square with a given size.
 
@@ -94,13 +74,10 @@ class Square(Rectangle):
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
-
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
-
         """Returns the area of the square."""
-
         return self.__size * self.__size
