103191ffdf74daba670f02c107d64f803cf047b4
Thea33710 <thea33710@gmail.com>
2025-09-26 10:34:41 +0200

    task 1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -50,6 +50,8 @@ class Rectangle(Shape):
 
     def perimeter(self):
         """The perimeter of the Rectangle."""
+        if self.width == 0 or self.height == 0:
+            return 0
         return 2 * (self.width + self.height)
 
 
