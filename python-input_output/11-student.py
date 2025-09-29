f4003fa1e35fdd31ebdc1bcca74f9634aee03268
Thea33710 <thea33710@gmail.com>
2025-09-29 21:50:10 +0200

    task 9 12

---
+++ python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""A module with a class Student."""
+
+
+class Student:
+    """A student class."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns a dictionary of the student."""
+        if isinstance(attrs, list) and all(isinstance(attrs, str) for attr in attrs):
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Replace all attributes of the Student
+        instance with those in json dictionary.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
