bcea259ff5a7ba9ebd6971cae7db5b2567dd96e2
Thea33710 <thea33710@gmail.com>
2025-10-22 18:23:29 +0200

    test

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -25,8 +25,8 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
     query = (
-        "SELECT * FROM states"
-        "WHERE name = '{}'"
+        "SELECT * FROM states "
+        "WHERE name = '{}' "
         "ORDER BY id ASC".format(state_name)
     )
     cursor.execute(query)
